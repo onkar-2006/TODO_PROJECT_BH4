880788dee5e94bfdc48a6ed61d13e8d082bb83c0
onkar kshirsagar <onkarkshirsagar297@gmail.com>
2026-03-17 11:42:06 +0530

    pushing the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,11 @@ import Home from './components/TodoComponents/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/signin" element={<Login />} /> 
-        <Route path="/home" element={<Home />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Login />} /> {/* Add this line */}
+      <Route path="/register" element={<Register />} />
+      <Route path="/signin" element={<Login />} /> 
+      <Route path="/home" element={<Home />} />
+    </Routes>
     </BrowserRouter>
 );
