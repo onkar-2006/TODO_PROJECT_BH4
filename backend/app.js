c9c8ba06db4270d640fe0c712d2dd32fc34abf7b
onkar kshirsagar <onkarkshirsagar297@gmail.com>
2026-03-17 11:49:00 +0530

    fixing the error

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173' , 'https://todo-project-bh-4.vercel.app/'],
+  origin: ['http://localhost:5173' , 'https://todo-project-bh-4.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
